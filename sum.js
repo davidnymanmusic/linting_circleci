db65e5b28d2b0fbadeb9fc803aada574ad2fd04a
David Nyman <davenyman@gmail.com>
2018-03-13 14:47:13 -0400

    add sum
    adding a sum function

---
+++ sum.js
@@ -0,0 +1,15 @@
+function myTwoSum(arr, value) {
+  const indices = [];
+
+  for (let i = 0; i < arr.length; i + 1) {
+    for (let j = i + 1; j < arr.length; j + 1) {
+      if (arr[i] + arr[j] === value) {
+        indices.push([arr[i], arr[j]]);
+      } else {
+        null;
+      }
+    }
+  }
+}
+
+module.exports = myTwoSum;
